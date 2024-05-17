cf0e386401f852c13570d8ad04309ed07c2b5697
SeungjunHa <gkak1121@naver.com>
2024-05-17 13:46:57 +0900

    [feature] 26 27 [refactoring] 28

--- Project4/Baseball.cpp
+++ Project4/Baseball.cpp
@@ -1,1 +1,17 @@
-#include "Baseball.h"
+#pragma once
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+class Baseball
+{
+public:
+	void guess(const string& string) {
+		if (string.length() != 3) {
+			throw length_error("Must be three letters.");
+		}
+	}
+private:
+};
+
